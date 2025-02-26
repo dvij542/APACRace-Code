b3af1e8e21156b2c34a9dfcf89d92d229df1f184
dvij542 <dvij.kalaria@gmail.com>
2025-02-25 16:01:12 -0800

    Added raceline for carla

--- apacrace/raceline/extract_and_save.py
+++ apacrace/raceline/extract_and_save.py
@@ -2,11 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
-saved_raceline = np.loadtxt('/home/dvij/bayesrace/bayes_race/global_racetrajectory_optimization/outputs/traj_race_cl.csv',delimiter=';')
+saved_raceline = np.loadtxt('/home/dvij/APACRace-Code/apacrace/global_racetrajectory_optimization/outputs/traj_race_cl.csv',delimiter=';')
 # print(saved_raceline.shape)
 x = saved_raceline[:,1]
 y = saved_raceline[:,2]
 v = saved_raceline[:,5]
 time = saved_raceline[:,0]
-np.savez('/home/dvij/bayesrace/bayes_race/tracks/src/carla_raceline.npz',x=x,y=y,speed=v,time=time,inputs=time)
+np.savez('/home/dvij/APACRace-Code/apacrace/tracks/src/carla_raceline.npz',x=x,y=y,speed=v,time=time,inputs=time)
 # print(saved_raceline['y_ei'].shape)
---
+++ apacrace/tracks/src/reformat_for_raceline.py
@@ -0,0 +1,10 @@
+import numpy as np
+import math
+raceline = np.loadtxt('carla_center.txt', delimiter=',').T
+
+raceline_new = np.zeros((raceline.shape[0], 4))
+raceline_new[:, :2] = raceline
+raceline_new[:, 2] = 1.5
+raceline_new[:, 3] = 1.5
+
+np.savetxt('carla.csv', raceline_new, delimiter=',', fmt='%.3f')
